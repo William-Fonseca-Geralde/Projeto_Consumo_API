5e33b0b33f102ea7118d07daa7faa1f9ddac21ad
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-03-30 13:09:23 -0300

    adição da função de descobrir os países

--- consumoAPI.py
+++ consumoAPI.py
@@ -6,6 +6,7 @@
 from deep_translator import GoogleTranslator
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from PIL import ImageTk, Image
 import requests
 
@@ -21,13 +22,50 @@ class CountryLanguage:
       dado = response.json()
       return dado
     else:
-      return "ERRO"
+      return "Esse idioma não existe ou\n não tem país com esse idioma como oficial"
 
 def mostrarDados():
-  lingua = ""
+  lingua = resposta.get()
   paises = CountryLanguage(lingua)
   dados = paises.consumirAPI()
+  if isinstance(dados, list):
+    dadosView.delete(*dadosView.get_children())
+    for pais in dados:
+      if 'capital' in pais:
+        capital = pais['capital']
+        if type(capital) == list and len(capital) > 1:
+          capi = ""
+          for idx, cap in enumerate(capital):
+            if len(capital) - 1 != idx:
+              capi += cap + ", "
+            else:
+              capi += cap
+          capital = capi
+        if type(capital) == list:
+          capital = capital[0]
+      else:
+        capital = "Não tem"
+      info = (
+        pais['name']['common'],
+        pais['name']['official'],
+        capital
+      )
+      dadosView.insert('', tk.END, values=info)
+      info = ()
+    for nomes in colunas:
+      dadosView.heading(nomes, text=nomes)
+  else:
+    messagebox.showinfo(title="Erro do Programa", message=dados)
+    
+def centralizar_tela(tela):
+  screen_width = tela.winfo_screenwidth()
+  screen_height = tela.winfo_screenheight()
+  
+  x = (screen_width - tela.winfo_reqwidth()) // 6
+  y = (screen_height - tela.winfo_reqheight()) // 8
 
+  tela.geometry("+{}+{}".format(x, y))
+      
 janela = tk.Tk()
 janela.title("Projeto Consumo de API")
 janela.configure(bg="#f0f0f0")
@@ -36,36 +74,44 @@ frame = ttk.Frame(janela)
 frame.pack()
 
 texto = tk.LabelFrame(frame, text="Digite o idioma abaixo", fg= "black")
-texto.grid(row=0, column=0, padx=5, pady=5)
-
-img = Image.open("mundo.png")
-nv_img = img.resize(size=(200, 75))
-
-imagem = ImageTk.PhotoImage(nv_img)
-imgFrame = tk.Label(frame, image=imagem)
-imgFrame.grid(row=1, column=0, padx=5, pady=5)
+texto.grid(row=1, column=0, padx=5, pady=5)
 
 resposta = ttk.Entry(texto)
-resposta.insert(0, "Idioma...")
+resposta.insert(0, "Idioma")
 resposta.bind("<FocusIn>", lambda e: resposta.delete('0', 'end'))
-resposta.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
+resposta.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
 
-botao = ttk.Button(texto, text="Enviar".upper())
-botao.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+botao = ttk.Button(texto, text="Enviar".upper(), command=mostrarDados)
+botao.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
+
+img = Image.open("mundo.png")
+nv_img = img.resize(size=(210, 110))
+
+imagem = ImageTk.PhotoImage(nv_img)
+imgFrame = tk.Label(texto, image=imagem)
+imgFrame.grid(row=2, column=0,columnspan=2, padx=5, pady=5)
+
+explicacao = tk.LabelFrame(texto, text="Explicação", fg="black")
+explicacao.grid(row=1, rowspan=2, column=2, padx=45, pady=15)
+
+textoExplic = tk.Text(explicacao, wrap="word", width=20, height=7)
+textoExplic.grid(row=1, column=1, padx=10, pady=10)
+textoExplic.insert(tk.END, "Nesse programa vai ser mostrado todos os países que tem o idioma oficial que você escrever na caixinha ao lado")
 
 dadosFrame = ttk.Frame(frame)
-dadosFrame.grid(row=0, column=1, padx=(0, 20), pady=10)
+dadosFrame.grid(row=3, column=0, padx=20, pady=10)
 scrollDados = ttk.Scrollbar(dadosFrame)
 scrollDados.pack(side="right", fill="y")
 
 colunas = ("Nome do país", "Nome Oficial", "Capital")
-dadosView = ttk.Treeview(dadosFrame, show="headings", yscrollcommand=scrollDados.set, columns=colunas, height=15)
+dadosView = ttk.Treeview(dadosFrame, show="headings", yscrollcommand=scrollDados.set, columns=colunas, height=20)
 
-dadosView.column("Nome do país", width=100)
-dadosView.column("Nome Oficial", width=150)
-dadosView.column("Capital", width=150)
-dadosView.pack()
+dadosView.column("Nome do país", width=200)
+dadosView.column("Nome Oficial", width=500)
+dadosView.column("Capital", width=270)
+dadosView.pack(fill="both", expand=True)
 scrollDados.config(command=dadosView.yview)
 
+centralizar_tela(janela)
 
 janela.mainloop()
